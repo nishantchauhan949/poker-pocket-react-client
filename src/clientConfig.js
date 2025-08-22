43982f19b61b826f8410aaf16ee52b55f3dc6b1c
Nishant <nishantchauhan949@hotmail.com>
2025-08-23 02:23:25 +0530

    prettier was crying so had to shorten the line and changed env variable name

--- src/clientConfig.js
+++ src/clientConfig.js
@@ -4,7 +4,7 @@ const localIP = window.location.hostname;
 
 const config = {
   isProduction,
-  socketURI: process.env.REACT_APP_BACKEND_WEB_SOCKET_URL,
+  socketURI: process.env.REACT_APP_WS_URL,
 };
 
 export default config;
