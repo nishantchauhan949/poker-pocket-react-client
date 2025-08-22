446c78325fc1fbee87e24823d8abae52535559f3
Nishant <nishantchauhan949@hotmail.com>
2025-08-23 01:40:47 +0530

    environment variables prefixed with REACT_APP_ are automatically made available to the browser in Create React App projects

--- src/clientConfig.js
+++ src/clientConfig.js
@@ -4,7 +4,7 @@ const localIP = window.location.hostname;
 
 const config = {
   isProduction,
-  socketURI: process.env.BACKEND_SOCKET_URL || `ws://${localIP}:8000/api`,
+  socketURI: process.env.REACT_APP_BACKEND_WEB_SOCKET_URL || `ws://${localIP}:8000/api`,
 };
 
 export default config;
